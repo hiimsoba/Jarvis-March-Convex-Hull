2057b58e02f4033bf05d54cd56ef64c7e675654f
hiimsoba <35265430+hiimsoba@users.noreply.github.com>
2018-12-27 13:20:22 +0200

    updated code and added comments

--- point.js
+++ point.js
@@ -1,7 +1,12 @@
 class point {
-  // keeps the coordinates of a point
-  constructor(x, y) {
-    this.x = x ;
-    this.y = y ;
+  // keeps the coordinates of a point... best class in this whole thing.
+  constructor(x = random(width), y = random(height)) {
+    this.x = x;
+    this.y = y;
+  }
+  // render the point
+  // set the stroke only once before calling render on all of the points to avoid useless calls to the stroke / fill functions
+  render() {
+    ellipse(this.x, this.y, 2, 2);
   }
 }
